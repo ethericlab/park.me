62b57e4a43fe9f48fd31f4e997fe7d958ca561b4
Nikita Kurpas <nikitakurpas@gmail.com>
2019-05-05 08:40:49 +0200

    Add types

---
+++ src/types/common.ts
@@ -0,0 +1,40 @@
+export type DatePickerType = 'start' | 'end'
+
+export type Feature =
+  | 'instant'
+  | 'cover'
+  | 'lit'
+  | 'charge'
+  | 'cam'
+  | 'gate'
+  | 'guard'
+  | 'allDay'
+  | 'night'
+  | 'heated'
+
+export type Features = { [P in Feature]: boolean }
+
+export type ParkingSpotOwner = {
+  name: string
+  verified: boolean
+}
+
+export type PriceModel = 'hourly' | 'daily' | 'monthly' | 'yearly'
+
+export type Location = {
+  lat: number,
+  lng: number
+}
+
+export type ParkingDetails = {
+  owner: ParkingSpotOwner,
+  address: string,
+  pricing: {
+    model: PriceModel,
+    price: number,
+    currency: string
+  }[],
+  description: string,
+  features: Feature[],
+  location: Location
+}
