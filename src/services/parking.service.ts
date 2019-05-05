89f31baf395b8ecbad5785f422a5378d23ae3e17
Ruslan Bakeyev <ruslan.bakeyev@etheric.design>
2019-05-05 11:38:10 +0200

    Merge changes

--- src/services/parking.service.ts
+++ src/services/parking.service.ts
@@ -1,6 +1,6 @@
 import parkingLocations from '../backend/parkings-location.json'
 import { ParkingType } from '../constants'
-import {PrivateParking, PublicParking} from "../types/common";
+import { PrivateParking, PublicParking } from '../types/common'
 
 const zoneToParkingMap = {
   1: ParkingType.RESIDENTS,
@@ -11,18 +11,20 @@ const zoneToParkingMap = {
 export const getParkingPolygons = (ids: string[]): PublicParking[] => {
   return ids.map(id => {
     const parking = (parkingLocations as any)[id]
-    const coordinates = parking.geometry.coordinates[0].map(([longitude, latitude]: [number, number]) => ({
-      latitude,
-      longitude
-    }))
+    const coordinates = parking.geometry.coordinates[0].map(
+      ([longitude, latitude]: [number, number]) => ({
+        latitude,
+        longitude
+      })
+    )
     const parkingType: ParkingType = (zoneToParkingMap as any)[parking.properties['TYPZONY']]
     const numOfPlaces = parking.properties['PS_ZPS']
 
     return {
       type: 'public',
       id: parking.properties['ZPS_ID'],
-      coordinates,
       parkingType,
+      coordinates,
       capacity: numOfPlaces
     }
   })
